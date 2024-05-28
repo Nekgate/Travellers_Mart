9d88e6ac0528f4beffd8f24db5f8e23343fad8b5
Nekgate <Nekgate@gmail.com>
2024-05-28 08:05:00 +0100

    Update and Delete fuction added

--- Flask_Mart/flask_station/models.py
+++ Flask_Mart/flask_station/models.py
@@ -23,8 +23,8 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     selling_item = db.Column(db.String(100), nullable=False,)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     price = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
--- Flask_Mart/flask_station/routes.py
+++ Flask_Mart/flask_station/routes.py
@@ -1,31 +1,17 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flask_station import app, db, bcrypt
-from flask_station.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask_station.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_station.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-posts = [
-    {
-        'merchant': 'Benedict Nathaniel',
-        'selling_item': 'Wrist Watch',
-        'price': 'Price $500',
-        'date_posted': 'April 6th, 2024'
-    },
-    {
-        'merchant': 'Richard Bonk',
-        'selling_item': 'Quality Glass',
-        'price': 'Price $200',
-        'date_posted': 'April 4th, 2024'
-    }
-]
-
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -122,3 +108,56 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(selling_item=form.title.data, price=form.content.data, merchant=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your Post has been updated!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Item', 
+                           form=form, legend='New Item')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title='post.title', post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.merchant != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.selling_item = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your Item has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.selling_item
+        form.content.data = post.price
+    return render_template('create_post.html', title='Update Item',
+                           form=form, legend='Update Post')
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.merchant != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your Item has been deleted!', 'success')
+    return redirect(url_for('home'))
+    
