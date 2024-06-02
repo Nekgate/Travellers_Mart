c3d46d5a20f421255749ff8722f69f1c88430fca
Ana123tase <anastasedukunda@gmail.com>
2024-06-02 16:20:48 -0400

    working on sales and selling

--- Flask_Mart/flask_station/routes.py
+++ Flask_Mart/flask_station/routes.py
@@ -5,7 +5,7 @@ from flask import render_template, url_for, flash, redirect, request, abort
 from flask_station import app, db, bcrypt, mail
 from flask_station.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
                                  PostForm, RequestResetForm, ResetPasswordForm, SellingForm)
-from flask_station.models import User, Post
+from flask_station.models import User, Post, Product_for_sale
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_mail import Message
 
